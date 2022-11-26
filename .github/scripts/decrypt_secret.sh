fcb9c25033511e3297056287b5d604e25ec14596
j-wyatt-a <53614528+j-wyatt-a@users.noreply.github.com>
2022-11-26 23:30:42 +0800

    Update decrypt_secret.sh

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -2,5 +2,4 @@
 
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $GITHUB_WORKSPACE/android/app/my-release-key.keystore $GITHUB_WORKSPACE/.github/scripts/my-release-key.keystore.gpg
+cp $GITHUB_WORKSPACE/.github/scripts/my-release-key.keystore $GITHUB_WORKSPACE/android/app/my-release-key.keystore 
